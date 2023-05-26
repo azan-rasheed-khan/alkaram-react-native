36958227550f4bb06f887dfc4d4992b434dbaa2f
Azaan Khan <azaankhan271@gmail.com>
2023-05-26 20:38:39 +0500

    header and footer updated

---
+++ src/modules/footer/footer.js
@@ -0,0 +1,22 @@
+import { ScrollView, StyleSheet, View } from "react-native"
+
+
+
+const Footer = () => {
+    return (
+        <ScrollView>
+            <View>
+
+            </View>
+        </ScrollView>
+    )
+}
+
+export default Footer
+
+
+const styles = StyleSheet.create({
+    container : {
+        
+    }
+})
--- src/modules/header/Header.js
+++ src/modules/header/Header.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     headerDiv : {
         width:'100%',
         height:1000,
-        backgroundColor:"yellow",
+        backgroundColor:"green",
   }
   });
 
