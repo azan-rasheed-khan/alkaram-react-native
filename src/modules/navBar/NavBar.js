281206881804176b2f34959bb2b1d5ce7b6236ca
Azaan Khan <azaankhan271@gmail.com>
2023-10-04 16:32:46 +0500

    new screens added

---
+++ src/modules/navBar/NavBar.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+  TextInput
+} from 'react-native';
+
+const NavBar = () => {
+    return(
+        <View>
+            <Text>
+                huda
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerDiv : {
+        width:'100%',
+        height:1000,
+        backgroundColor:"green",
+  },
+  container : {
+    flex : 1
+  }
+  });
+
+export default NavBar
