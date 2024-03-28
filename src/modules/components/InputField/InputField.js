15934f03dc1687f5ba789fcbe40660f82d44da28
Azaan Khan <azaankhan271@gmail.com>
2024-03-28 17:23:14 +0500

    login functionality fixing

--- src/modules/components/InputField/InputField.js
+++ src/modules/components/InputField/InputField.js
@@ -1,21 +1,25 @@
-import React from "react"
-import { StyleSheet, TextInput, View , Text } from "react-native"
+import React from 'react';
+import {StyleSheet, TextInput, View, Text} from 'react-native';
 
-
-const InputField = (props) => {
-    return (
+const InputField = props => {
+  return (
     <View>
-        <Text style={styles.inputFieldName}>{props.fieldName}</Text>
-        <TextInput secureTextEntry={props.secureTextEntry} keyboardType={props.keyboardType} style={props.textInputStyles} placeholder={props.placeholder}/>
+      <Text style={styles.inputFieldName}>{props.fieldName}</Text>
+      <TextInput
+        onChangeText={props.onChangeText}
+        secureTextEntry={props.secureTextEntry}
+        keyboardType={props.keyboardType}
+        style={props.textInputStyles}
+        placeholder={props.placeholder}
+      />
     </View>
-    ) 
-}
+  );
+};
 
-export default InputField
-
+export default InputField;
 
 const styles = StyleSheet.create({
-    inputFieldName : {
-        marginBottom : 10
-    }
-})
+  inputFieldName: {
+    marginBottom: 10,
+  },
+});
